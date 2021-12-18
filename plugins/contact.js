5a1b8dcff972d477fc6d80061b2ed5b54e050519
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2021-12-18 20:41:39 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by aslu
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amalser [OWNER]\n' // full name
+            + 'FN:Tedzo [OWNER]\n' // full name
             + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=917736835721:+91 7736835721\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "Tedzo [OWNER]", vcard: vcard}, MessageType.contact)
 }))
